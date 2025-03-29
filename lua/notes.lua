b9343842248b65668841fc59da24daeb6d2ba4d9
Sune Simonsen <sune@we-knowhow.dk>
2025-03-29 19:52:01 +0100

    Initial notes module

---
+++ lua/notes.lua
@@ -0,0 +1,71 @@
+local Notes = {}
+
+Notes.find_note = function(opts)
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+
+  local function run_selection(prompt_bufnr)
+    actions.select_default:replace(function()
+      actions.close(prompt_bufnr)
+      local selection = action_state.get_selected_entry()
+
+      if selection then
+        vim.cmd("e " .. opts.dir .. "/" .. selection[1])
+      else
+        local timestamp = os.date("!%Y%m%dT%H%M%S", os.time())
+
+        local title = action_state.get_current_line()
+
+        title = title:lower()
+        title = title:gsub(" ", "-")
+        title = title:gsub("-+", "-")
+        title = title:gsub("[^-0-9a-zæøå]", "")
+
+        local filename = timestamp .. "--" .. title .. ".md"
+        vim.cmd("e " .. opts.dir .. "/" .. filename)
+      end
+    end)
+    return true
+  end
+
+  require("telescope.builtin").find_files({
+    cwd = opts.dir,
+    attach_mappings = run_selection,
+  })
+end
+
+Notes.search_notes = function(opts)
+  require("telescope.builtin").live_grep({ cwd = opts.dir })
+end
+
+Notes.link_to_note = function(opts)
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+
+  local function run_selection(prompt_bufnr)
+    actions.select_default:replace(function()
+      local selection = action_state.get_selected_entry()
+
+      if selection then
+        actions.close(prompt_bufnr)
+
+        local filename = selection[1]
+        local id = filename:sub(1, 15)
+        local title_end = filename:find("[_.]", 18)
+        local title = filename:sub(18, title_end - 1):gsub("-", " ")
+
+        vim.api.nvim_put({ "[" .. title .. "](" .. id .. ".id)" }, "c", true, true)
+      else
+        print("No file selected")
+      end
+    end)
+    return true
+  end
+
+  require("telescope.builtin").find_files({
+    cwd = opts.dir,
+    attach_mappings = run_selection,
+  })
+end
+
+return Notes
