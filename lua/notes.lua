c9fb854865b0638240093a360852f64fafb6f62e
Sune Simonsen <sune@we-knowhow.dk>
2025-03-30 11:28:48 +0200

    Clean tags

--- lua/notes.lua
+++ lua/notes.lua
@@ -1,22 +1,40 @@
 local Notes = {}
 
-Notes.get_filename = function(opts)
-  local timestamp = os.date("!%Y%m%dT%H%M%S", opts.timestamp or os.time())
-
-  local title = opts.title
-
+local function clean_title(title)
   title = title:lower()
   title = title:gsub(" ", "-")
   title = title:gsub("-+", "-")
   title = title:gsub("[^-0-9a-zæøå]", "")
+  return title
+end
+
+local function clean_tag(tag)
+  tag = tag:lower()
+  tag = tag:gsub(" ", "-")
+  tag = tag:gsub("_+", "_")
+  tag = tag:gsub("[^_0-9a-zæøå]", "")
+  return tag
+end
+
+local get_filename = function(opts)
+  local timestamp = os.date("!%Y%m%dT%H%M%S", opts.timestamp or os.time())
+
+  local title = clean_title(opts.title)
 
   local filename = timestamp .. "--" .. title
 
-  local tags = opts.tags or {}
+  local tags = {}
+
+  for i, tag in ipairs(opts.tags or {}) do
+    tags[i] = clean_tag(tag)
+  end
+
+  table.sort(tags)
+
   if 0 < #tags then
     filename = filename .. "_"
     for _, tag in ipairs(tags) do
-      filename = filename .. "_" .. tag
+      filename = filename .. "_" .. clean_tag(tag)
     end
   end
 
@@ -39,7 +57,7 @@ Notes.find_note = function(opts)
       else
         local line = action_state.get_current_line()
         local parts = vim.fn.split(line, "\\s*,\\s*")
-        local filename = Notes.get_filename({
+        local filename = get_filename({
           dir = opts.dir,
           title = parts[1],
           tags = { table.unpack(parts, 2) },
